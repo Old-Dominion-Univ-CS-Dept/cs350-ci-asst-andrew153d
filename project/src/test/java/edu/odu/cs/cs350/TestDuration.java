89b9f3343f3e2c24fbeadc7f75531d5dba0da931
cs_agrow002 <cs_agrow002@callisto.infra.cs.odu.edu>
2022-11-10 14:53:46 -0500

    add test

--- project/src/test/java/edu/odu/cs/cs350/TestDuration.java
+++ project/src/test/java/edu/odu/cs/cs350/TestDuration.java
@@ -173,6 +173,10 @@ public class TestDuration {
 	}
 	*/
 	@Test
+		public final void easyPass2() {
+ 	 	assertEquals(1,1);
+	}
+	@Test
 		public final void easyPass1() {
  	 	assertEquals(1,1);
 	}
